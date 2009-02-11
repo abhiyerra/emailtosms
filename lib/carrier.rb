d029c2d065f2278e4fb4c81eaa2e21dd14574695
Kesava Yerra <abhi@traytwo.com>
2009-02-10 17:08:22 -0800

    Code added to format message

---
+++ lib/carrier.rb
@@ -0,0 +1,14 @@
+module Carrier
+  CARRIERS = {
+    "AT&T" => "txt.att.net",
+    "Verizon" => "vtext.com",
+    "T-Mobile" => "tmomail.net",
+    "Sprint" => "messaging.sprintpcs.com",
+    "Nextel" => "messaging.nextel.com"
+    "Virgin Mobile" => "vmobl.com",
+  }
+
+  def self.get_email contact
+    "#{contact[:number]}@#{CARRIERS[contact[:carrier]]}"
+  end
+end
--- lib/emailtosms.rb
+++ lib/emailtosms.rb
@@ -1,3 +1,28 @@
-class Emailtosms
-  VERSION = '1.0.0'
+class EmailToSMS
+  VERSION = '0.1.0'
+
+  @smtp = nil
+  @from = 'EmailToSMS@example.com'
+
+  def initialize smtp, from
+    @smtp = smtp
+    @from = from
+  end
+
+  def send_sms msg, subject="", contacts
+    msg = "From: #{@from}\n"
+
+    to = contacts == Array ?
+         contacts.collect { |c| Carrier::get_email(c) } :
+         Carrier::get_email(c)
+
+    msg << "To: #{to.join(', ')}"
+    msg << "Subject: #{subject}\n" unless subject.empty?
+    msg << "\n"
+    msg << "#{msg}" 
+
+    @smtp.start do |smtp|
+      smtp.send_message msg, from, to
+    end
+  end
 end
