083c9876af5e4eb1ef196011de7ffc6fe3c4aced
Kesava Yerra <abhi@traytwo.com>
2009-02-10 21:28:50 -0500

    Moved files around. Messages now send properly

--- lib/emailtosms.rb
+++ lib/emailtosms.rb
@@ -1,34 +1,31 @@
-require 'carrier'
+require 'emailtosms/carrier'
 
 class EmailToSMS
   VERSION = '0.1.0'
-
-  @smtp = nil
-  @from = 'EmailToSMS@example.com'
 
   def initialize smtp, from
     @smtp = smtp
     @from = from
   end
 
-  def send_sms msg, subject, contacts
+  def send_sms message, subject, contacts
     msg = "From: #{@from}\n"
 
     to = nil
     if contacts == Array 
       to = contacts.collect { |c| Carrier::get_email(c) } 
-      msg << "To: #{to.join(', ')}"
+      msg << "To: #{to.join(', ')}\n"
     else
       to = Carrier::get_email(contacts)
-      msg << "To: #{to}"
+      msg << "To: #{to}\n"
     end
 
-    msg << "Subject: #{subject}\n" unless subject.nil?
+    msg << "Subject: #{subject}\n" if subject
     msg << "\n"
-    msg << "#{msg}" 
+    msg << "#{message}\n" 
 
     @smtp.start do |smtp|
-      smtp.send_message msg, from, to
+      smtp.send_message msg, @from, to
     end
   end
 end
