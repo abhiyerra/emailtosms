2c932b0a5be4404a1d631377136c469186736b0b
Kesava Yerra <abhi@traytwo.com>
2009-02-10 21:57:51 -0500

    The format of sending messages is much nicer

--- lib/emailtosms.rb
+++ lib/emailtosms.rb
@@ -1,22 +1,25 @@
 require 'emailtosms/carrier'
 
 class EmailToSMS
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
 
   def initialize smtp, from
     @smtp = smtp
     @from = from
   end
 
-  def send_sms message, subject, contacts
+  def send_sms(*args)
+    to = args.first[:to]
+    subject = args.first[:subject]
+    message = args.first[:message]
+
     msg = "From: #{@from}\n"
 
-    to = nil
-    if contacts == Array 
-      to = contacts.collect { |c| Carrier::get_email(c) } 
+    if to.class == Array 
+      to = to.collect { |c| Carrier::get_email(c) } 
       msg << "To: #{to.join(', ')}\n"
     else
-      to = Carrier::get_email(contacts)
+      to = Carrier::get_email(to)
       msg << "To: #{to}\n"
     end
 
